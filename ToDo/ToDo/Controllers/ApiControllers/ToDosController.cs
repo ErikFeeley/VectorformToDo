2c54cac2cc6bf5a6dc5d8c20e9ee1387c25e778a
ErikFeeley <ehfeeley@gmail.com>
2016-07-03 18:26:39 -0400

    Wiring up the ToDos api, currently working on restfullish way to delete from multiple ids

---
+++ ToDo/ToDo/Controllers/ApiControllers/ToDosController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDo.Models;
+
+namespace ToDo.Controllers.ApiControllers
+{
+    public class ToDosController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public ToDosController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateToDo(Todo newToDo)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _context.Todos.Add(newToDo);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" + newToDo.Id), newToDo);
+        }
+
+        [HttpPut]
+        public IHttpActionResult UpdateToDo(int id, Todo toDo)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var toDoInDb = _context.Todos.SingleOrDefault(t => t.Id == id);
+
+            if (toDoInDb == null)
+                return NotFound();
+
+            toDoInDb.IsComplete = toDo.IsComplete;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteToDo(int id)
+        {
+            var toDoInDb = _context.Todos.SingleOrDefault(t => t.Id == id);
+
+            if (toDoInDb == null)
+                return NotFound();
+
+            _context.Todos.Remove(toDoInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // how to do a restful delete of a range... hmm
+    }
+}
--- ToDo/ToDo/Scripts/App/App.js
+++ ToDo/ToDo/Scripts/App/App.js
@@ -1,5 +1,32 @@
 ﻿var toDoApp = angular.module('toDoApp', []);
 
-toDoApp.controller('homeController', ['$scope', function ($scope) {
+toDoApp.controller('homeController', ['$scope', '$http', function ($scope, $http) {
   $scope.toDos = [];
+  $scope.newToDo = {};
+
+  $scope.addToDo = function (description) {
+    $http.post('/api/ToDos', $scope.newToDo).then(function (response) {
+      $scope.toDos.push(response.data);
+      $scope.newToDo.description = '';
+    });
+  }
+
+  $scope.deleteToDo = function (toDo) {
+    $http.delete('/api/ToDos/' + toDo.id).then(function (response) {
+      var index = $scope.toDos.indexOf(toDo);
+      $scope.toDos.splice(index, 1);
+    });
+  }
+
+  $scope.completeToDo = function (toDo) {
+    toDo.isComplete = true;
+    $http.put('/api/ToDos/' + toDo.id, toDo).then(function (response) {
+    });
+  }
+
+  $scope.activateToDo = function (toDo) {
+    toDo.isComplete = false;
+    $http.put('/api/ToDos/' + toDo.id, toDo).then(function (response) {
+    });
+  }
 }]);
